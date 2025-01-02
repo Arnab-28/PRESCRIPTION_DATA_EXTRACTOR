07c2fc8e4bb684df5b224288ff157de243cff3a3
Arnab Das <70911619+Arnab-28@users.noreply.github.com>
2025-01-02 12:35:58 +0530

    PRESCRIPTION_DATA_EXTRACTOR.py

--- PRESCRIPTION_DATA_EXTRACTOR.py
+++ PRESCRIPTION_DATA_EXTRACTOR.py
@@ -323,11 +323,11 @@ if uploaded_file:
                 # Display the cleaned response in a text area, allowing the user to edit
                 st.session_state["edited_text"] = st.text_area("Extracted Data (editable)", cleaned_response, height=200)
 
-                if st.button("Save Edited Extracted Data"):
-                    # Update session state with edited text for download
-                    st.session_state["extracted_text"] = st.session_state["edited_text"]
+                #if st.button("Save Edited Extracted Data"):
+                # Update session state with edited text for download
+                st.session_state["extracted_text"] = st.session_state["edited_text"]
     
-                    st.download_button("Download Edited Extracted Data (.txt)", st.session_state["edited_text"], file_name="extracted_data.txt", mime="text/plain")
+                st.download_button("Download Edited Extracted Data (.txt)", st.session_state["edited_text"], file_name="extracted_data.txt", mime="text/plain")
 
 # Upload the processed text file
 uploaded_text_file = st.file_uploader("Upload Extracted Text File", type=["txt"])
