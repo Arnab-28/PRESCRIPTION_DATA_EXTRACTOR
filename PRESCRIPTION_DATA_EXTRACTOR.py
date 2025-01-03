8325d9e693a766f4597dff200ae1741203e81582
Arnab Das <70911619+Arnab-28@users.noreply.github.com>
2025-01-03 23:50:41 +0530

    PRESCRIPTION_DATA_EXTRACTOR.py

--- PRESCRIPTION_DATA_EXTRACTOR.py
+++ PRESCRIPTION_DATA_EXTRACTOR.py
@@ -316,26 +316,19 @@ if uploaded_file:
     # Extract Data Button
     if st.button("Extract Information"):
         if image_part or pdf_text:
-            st.write("extracted_text")
             response = get_gemini_response(prompt, image_parts=image_part, pdf_text=pdf_text)
             if response:
-                st.write("extracted_text")
                 cleaned_response = clean_text(response)
-                st.write("extracted_text")
-
+                
             # Initialize session state for the edited text
             if "extracted_text" not in st.session_state:
-                st.write("extracted_text")
                 st.session_state["extracted_text"] = cleaned_response
-                st.write("extracted_text")
-        
+                
             # Display the cleaned response in a text area, allowing the user to edit
-            st.write("extracted_text")
             edited_text = st.text_area("Extracted Data (editable)", value=st.session_state["extracted_text"], height=200) 
             # key="extracted_text", on_change=download_edited_file
-            st.write("extracted_text")
-
-            st.download_button("Download Edited Extracted Data (.txt)",edited_text,file_name="extracted_data.txt",mime="text/plain")
+        
+        st.download_button("Download Edited Extracted Data (.txt)",edited_text,file_name="extracted_data.txt",mime="text/plain")
             
 # Upload the processed text file
 uploaded_text_file = st.file_uploader("Upload Extracted Text File", type=["txt"])
