2883d241a374684082af479c16bf6b7694697a52
Arnab Das <70911619+Arnab-28@users.noreply.github.com>
2025-01-04 00:18:19 +0530

    PRESCRIPTION_DATA_EXTRACTOR.py

--- PRESCRIPTION_DATA_EXTRACTOR.py
+++ PRESCRIPTION_DATA_EXTRACTOR.py
@@ -82,7 +82,7 @@ model = genai.GenerativeModel('gemini-1.5-flash')
 # Function to download the edited text file
 def download_edited_file():
     if "extracted_text" in st.session_state and st.session_state["extracted_text"]:
-        st.download_button("Download Edited Extracted Data (.txt)",edited_text,file_name="extracted_data.txt",mime="text/plain")
+        st.download_button("Download Edited Extracted Data (.txt)",st.session_state["extracted_text"],file_name="extracted_data.txt",mime="text/plain")
     else:
         st.warning("No data to download. Please edit the text first.")
 
@@ -325,8 +325,7 @@ if uploaded_file:
                 st.session_state["extracted_text"] = cleaned_response
          
             # Display the cleaned response in a text area, allowing the user to edit
-            edited_text = st.text_area("Extracted Data (editable)", value=st.session_state["extracted_text"], height=200, key="extracted_text", on_change=download_edited_file)
-            #st.download_button("Download Edited Extracted Data (.txt)",edited_text,file_name="extracted_data.txt",mime="text/plain")
+            st.text_area("Extracted Data (editable)", value=st.session_state["extracted_text"], height=200, key="extracted_text", on_change=download_edited_file)
             
 # Upload the processed text file
 uploaded_text_file = st.file_uploader("Upload Extracted Text File", type=["txt"])
